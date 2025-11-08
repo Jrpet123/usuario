3bc77173f2e9c1e22d7142cdcc82866be99ec83b
José Petrucio <jrazevedo.lps@gmail.com>
2025-11-08 11:50:09 -0300

    inclusao de metodo put de usuario

--- src/main/java/com/josepetrucio/usuario/business/UsuarioService.java
+++ src/main/java/com/josepetrucio/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.josepetrucio.usuario.infrastructure.entity.Usuario;
 import com.josepetrucio.usuario.infrastructure.exceptions.ConflictException;
 import com.josepetrucio.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.josepetrucio.usuario.infrastructure.repository.UsuarioRepository;
+import com.josepetrucio.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -48,6 +50,29 @@ public class UsuarioService {
     }
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        //aqui buscamos o email do usuario atraves do token (tirando a obrigatoriedade do email)
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+        //criptografa a senha sem conflitos de duplicidade de criptografia caso o user nao passe a senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        //busca os dados do usuario no db
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado " + email)
+                );
+
+        //mescla os dados da requisicao DTO com o db
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        //criptografa a senha novamente
+        //usuario.setSenha(passwordEncoder.encode(usuario.getPassword())); -> conflito de criptografias
+
+        //salva os dados convertidos e pega o retorno e converte para UsuarioDTO
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
 
     }
 }
